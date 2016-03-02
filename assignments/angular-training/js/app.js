bea56d4885b1376b848241d4ef2fe7b41827a528
sreedharan <sreedharan.namboodiry@claricetechnologies.com>
2016-03-02 11:54:25 +0530

    modified

--- assignments/angular-training/js/app.js
+++ assignments/angular-training/js/app.js
@@ -3,9 +3,9 @@
 var app = angular.module("expenseManager", []);
 
 app.controller("transactions", function ($scope, $http) {
-    //$http.get('http://localhost:1000/js/data.json').success(function (data) {
-    //    $scope.transactions = data;
-    //});
+    $http.get('http://localhost:1000/js/data.json').success(function (data) {
+        $scope.income = data;
+    });
     
     
     $scope.transactions = {
