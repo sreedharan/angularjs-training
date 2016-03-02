ed13bad6d86eff534e7f03dcf025126dffcb54f2
sreedharan <sreedharan.namboodiry@claricetechnologies.com>
2016-03-02 14:02:08 +0530

    mod

---
+++ assignments/angular-training/js/app/controllers/expense_controller.js
@@ -0,0 +1,12 @@
+
+
+var app = angular.module("expenseManager", []);
+
+(function(){
+    app.controller('ExpenseController' , ['$scope' , function($scope){
+        $scope.name = "mark";
+    }] )
+})
+});
+
+
